8ed781e017092f6f3e4e8240273424060656fd9f
Falgun-Dadhich07 <falgundadhich2007@gmail.com>
2026-02-21 02:50:22 +0530

    Add additional template directory for trading app

--- trading_system/trading_system/settings.py
+++ trading_system/trading_system/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = "trading_system.urls"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'trading/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'trading/templates'),os.path.join(BASE_DIR, 'trading/templates/trading')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
